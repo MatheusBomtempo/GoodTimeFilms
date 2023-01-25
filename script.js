4f64c726d77775d86bdb547006179b8c367fde03
Matheus Bomtempo <99459985+MatheusBomtempo@users.noreply.github.com>
2023-01-25 13:10:31 -0300

    tudo

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('header nav');
+
+menu.onclick = () =>
+{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+
+
+
+// media querry video
+
+
+
+
+
+
+
---
+++ scrollmagic.js
@@ -0,0 +1,35 @@
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearOptions={
+    threshold:1,
+    rootMargin: "0px 0px 450px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+ entries,
+ appearOnScroll
+){
+ entries.forEach(entry => {
+   if (!entry.isIntersecting) {
+     return;
+   } else {
+     entry.target.classList.add("appear");
+     appearOnScroll.unobserve(entry.target);
+   }
+    });
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+   });
+
+   sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+   });
+
+
+
+        
